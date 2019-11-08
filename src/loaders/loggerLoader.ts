ac903f60c29045ddc2bb89936bb37dcf2d175210
jdarrollador13 <jdesarrollador13@gmail.com>
2019-11-08 13:45:23 -0500

    build -- log

--- src/loaders/loggerLoader.ts
+++ src/loaders/loggerLoader.ts
@@ -1,28 +1,37 @@
 import winston from 'winston';
 import config from '../config';
-
+import  appRoot from 'app-root-path'
 const transports = [];
 if (process.env.NODE_ENV !== 'test') {
-    transports.push(
-        new winston.transports.File({
-            level: 'silly',
-            filename: `${__dirname}/../../app.log`,
-            handleExceptions: true,
-            maxsize: 5242880, // 5MB
-            maxFiles: 5
-        })
-    )
+  transports.push(
+    new winston.transports.File({
+      level: 'silly',
+      filename: `${appRoot.path}/app.log`,
+      handleExceptions: true,
+      maxsize: 5242880, // 5MB
+      maxFiles: 5
+    })
+  )
 } else {
-    transports.push(
+    /*transports.push(
         new winston.transports.Console({
             format: winston.format.combine(
                 winston.format.cli(),
                 winston.format.splat(),
             )
         })
-    )
+    )*/
+  transports.push(
+    new winston.transports.File({
+      level: 'silly',
+      filename: `${appRoot.path}/app.log`,
+      handleExceptions: true,
+      maxsize: 5242880, // 5MB
+      maxFiles: 5
+    })
+  )
 }
-
+//console.log('configurado logger', config.logs.level, winston.config.npm.levels);
 const LoggerInstance = winston.createLogger({
     level: config.logs.level,
     levels: winston.config.npm.levels,
