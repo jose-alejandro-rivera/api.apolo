552b80fafe91079e5191ef850807868d886a33f2
angela <angmora.7@hotmail.com>
2019-11-20 09:25:53 -0500

    prueba unitaria crear atencion funcionando

--- src/DAO/AtencionDAO.ts
+++ src/DAO/AtencionDAO.ts
@@ -19,7 +19,16 @@ export class AtencionDAO {
 			let { CodLogin, CodFlujo } = data;
 			const sqlGetSteps = await this.databaseConnection.getPool();
 			const result = await sqlGetSteps.query(`INSERT INTO Atencion (CodLogin, CodFlujo, Fecha) VALUES (${CodLogin},${CodFlujo},getdate()); SELECT SCOPE_IDENTITY() as Id_Atencion;`);
-			console.log(result.recordset);
+			return result.recordset;
+		} catch (error) {
+			return error
+		}
+	}
+
+	public async lastAtencion(){
+		try {
+			const sqlGetSteps = await this.databaseConnection.getPool();
+			const result = await sqlGetSteps.query(`SELECT TOP 1 CAST(Id_Atencion AS int) as Id_Atencion FROM Atencion ORDER BY Id_Atencion DESC `);
 			return result.recordset;
 		} catch (error) {
 			return error
@@ -29,8 +38,8 @@ export class AtencionDAO {
 	public async validateAtencion(CodLogin: number, CodFlujo: number){
 		try {
 			const sqlGetSteps = await this.databaseConnection.getPool();
-			let validateLogin = await sqlGetSteps.query`SELECT * FROM Login WHERE Id_Login = ${CodLogin}`;
-			let validateFlujo = await sqlGetSteps.query`SELECT * FROM Flujo WHERE Id_Flujo = ${CodFlujo}`;
+			let validateLogin = await sqlGetSteps.query`SELECT Id_Login, Usuario, Fecha FROM Login WHERE Id_Login = ${CodLogin}`;
+			let validateFlujo = await sqlGetSteps.query`SELECT Id_Flujo,NomFlujo,CodCategoriaFlujo,CodPaso_Inicial,Descripcion,Orden,Activo,Fecha,Usuario FROM Flujo WHERE Id_Flujo = ${CodFlujo}`;
 			if(validateLogin.recordset.length > 0 && validateFlujo.recordset.length > 0 ){
 				return true;
 			}else{
