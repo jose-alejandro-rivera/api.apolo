5324d6bcbd25b5788e3064e9f823710bf56142e7
angela <angmora.7@hotmail.com>
2019-11-25 14:49:30 -0500

    ajuste de validacion de BD

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -9,7 +9,7 @@ export default async ({ expressApp }: { expressApp: express.Application }) => {
     /**
      * se carga la base de datos
      */
-    Container.get(DatabaseConnection);
+    //Container.get(DatabaseConnection);
 
     /**
      * Se cargan todas las rutas
