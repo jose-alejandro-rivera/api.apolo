adb62be0cfa71d999ae20ec5840f8ee8b7769a09
jdarrollador13 <jdesarrollador13@gmail.com>
2019-12-05 14:18:06 -0500

    cobertura desde controller

--- unitTest/flujoList.test.ts
+++ unitTest/flujoList.test.ts
@@ -8,8 +8,9 @@ import FlujoController  from '../src/Controllers/FlujoController';
 test('getFlujoList retorna status 200 rows[data] de vuelve datos', async () => {
    let data1 = charge('flujosRequestObj');
 	 Container.bind(DatabaseConnection).to(DatabaseConnection).scope(Scope.Local);
-	 let flujoListDAO: FlujoListDAO = Container.get(FlujoListDAO);
-	 let data = await flujoListDAO.getFlujoList(1);
+   let flujoController:FlujoController = Container.get(FlujoController);
+	 //let flujoListDAO: FlujoListDAO = Container.get(FlujoListDAO);
+	 let data = await flujoController.getSteps(1);
    try{
      expect(data).toEqual(data1);
    }catch(error){
@@ -23,7 +24,7 @@ test('getFlujoList retorna status 201, rows [vacio] argumento invalido', async (
     "rows": []
   }
   Container.bind(DatabaseConnection).to(DatabaseConnection).scope(Scope.Local);
-  let flujoListDAO: FlujoListDAO = Container.get(FlujoListDAO);
+  let flujoListDAO:FlujoListDAO = Container.get(FlujoListDAO);
   let data = await flujoListDAO.getFlujoList(0)
   try{
     expect(data).not.toBe(data1);
@@ -38,7 +39,7 @@ test('getFlujoList retorna invalido en la repspuesta', async () => {
     "rows": []
   }
   Container.bind(DatabaseConnection).to(DatabaseConnection).scope(Scope.Local);
-  let flujoListDAO: FlujoListDAO = Container.get(FlujoListDAO);
+  let flujoListDAO:FlujoListDAO = Container.get(FlujoListDAO);
   let data = await flujoListDAO.getFlujoList(1);
   try{
     expect(data).toEqual(data1)
@@ -48,6 +49,6 @@ test('getFlujoList retorna invalido en la repspuesta', async () => {
 })
 
 test('verifica si hay variables no este definida', async () => {
-  let flujoController: FlujoController = Container.get(FlujoController);
+  let flujoController:FlujoController = Container.get(FlujoController);
   expect(await flujoController.getSteps(1)).toBeDefined();
 })
