af5ab378af9a63e6f85ac7f8b2d2e8abea6ed88b
angela <angmora.7@hotmail.com>
2019-12-09 16:46:48 -0500

    ajuste atencion

--- unitTest/atencion.test.ts
+++ unitTest/atencion.test.ts
@@ -42,14 +42,3 @@ test('ValidateAtencion should return true', async () => {
   let data = await atencionDAO.validateAtencion(1,1);
   expect(data).toEqual(true);
 });
-
-/*test('createAtencion validate response with Id_Atencion should return true', async () => {
-  Container.bind(DatabaseConnection).to(DatabaseConnection).scope(Scope.Local);
-  let atencionDAO: AtencionDAO = Container.get(AtencionDAO);
-  let inData = {
-    "CodLogin": 1,
-    "CodFlujo": 1
-  };
-  let data = await atencionDAO.createAtencion(inData);
-  expect(data).toContainEqual(expect.objectContaining({ Id_Atencion: expect.anything() }))
-});*/
