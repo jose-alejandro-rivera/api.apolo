3de04f83cda4e240a9ea61697ffac195c21534db
angela <angmora.7@hotmail.com>
2019-12-30 11:43:25 -0500

    compilacion ok

--- build/src/loaders/swaggerDocLoader.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
-const swagger_1 = require("../config/swagger/swagger");
-exports.swaggerSpec = swagger_jsdoc_1.default(swagger_1.swaggerOptions);
