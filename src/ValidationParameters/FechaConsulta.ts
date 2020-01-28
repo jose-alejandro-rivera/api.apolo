b753bcb14102bc16529b612ca5ebba95e328a7ca
desarrollador13 <jdesarrollador13@gmail.com>
2020-01-28 16:07:45 -0500

    ajustes servicios

---
+++ src/ValidationParameters/FechaConsulta.ts
@@ -0,0 +1,18 @@
+import moments  from 'moment'
+
+export default class FechaConsulta {
+	constructor(){}
+
+	getDateCurrent():string {
+		const fechaToconfig = new Date()
+    const fechaTo = moments(fechaToconfig).format("YYYY-MM-DD")
+    return fechaTo
+	}
+
+	getDatePass(): string {
+		const dateFromconfig = new Date()
+    dateFromconfig.setDate(dateFromconfig.getDate() - 30)
+    const fechaFrom = moments(dateFromconfig).format("YYYY-MM-DD")
+    return fechaFrom
+	}
+}
